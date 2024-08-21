e12ff882315332083ae39d968393c9bf2c2d510c
MidhatJalil <MIDHATJALIL345@GMAIL.COM>
2024-08-21 18:05:46 +0500

    Add files via upload

---
+++ Chapter#05/script.js
@@ -0,0 +1,163 @@
+// 1
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1+num2;
+// document.write(`Sum of 3 & 5 is ${sum} <br>`);
+
+// // 2
+// var diff = num1-num2;
+// document.write(`Difference of 3 & 5 is ${diff}<br>`);
+
+// var product = num1*num2;
+// document.write(`Product of 3 & 5 is ${product}<br>`);
+
+// var divide = num1/num2;
+// document.write(`Division of 3 & 5 is ${divide}<br>`);
+
+// var mod = num1%num2;
+// document.write(`Modulus of 3 & 5 is ${mod}<br>`);
+
+
+// // 3a
+// var number;
+// // 3b
+// document.write(`Value after variable declaration is:${number}<br>`);
+// // 3c
+// number = 5;
+// // 3d
+// document.write(`Initial value: ${number}<br>`);
+// // 3e
+// var number = ++number;
+// // 3f
+// document.write(`Value after increament is: ${number}<br>`);
+// // 3g
+// var number = number+7;
+// // 3h
+// document.write(`Value after addition is: ${number}<br>`);
+// // 3i
+// var number = --number;
+// // 3j
+// document.write(`Value after decreament is: ${number}<br>`);
+// // 3k
+// var number = number%3;
+// // 3l
+// document.write(`The Remainder is:${number}<br>`);
+
+
+// // 4
+// var ticketPrice = 600;
+// var cost = 5*ticketPrice;
+
+// document.write(`Total cost of buying 5 tickets to a movie is ${cost} PKR <br>`);
+
+// // 5
+
+// var num = 4;
+// var series = 1;
+
+// document.write(`Table of ${num} <br><br>`);
+// document.write(` ${num} x ${series} = ${num*series} <br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+// document.write(` ${num} x ${++series} = ${num*series}<br>`);
+
+// 6
+
+// var tempC = 25;
+// var Farenhiet = (tempC*9/5)+32;
+
+// var tempF = 70;
+// var Centigrade = (tempF-32)*5/9;
+
+// document.write(` ${tempC} C is ${Farenhiet}F <br>`);
+// document.write(` ${tempF} F is ${Centigrade}C <br>`);
+
+// 7
+
+// var itemPrice1 = 650;
+// var itemPrice2 = 100;
+// var itemQuan1 = 3;
+// var itemQuan2 = 7;
+// var shippingChrg = 100;
+// var totalCost = (itemPrice1*itemQuan1)+(itemPrice2*itemQuan2)+shippingChrg;
+
+// document.write(`<h1>Shopping Cart</h1>`);
+// document.write(`Price of item 1 is ${itemPrice1}<br>`);
+// document.write(`Quantity of item 1 is ${itemQuan1}<br>`);
+// document.write(`Price of item 2 is ${itemPrice2}<br>`);
+// document.write(`Quantity of item 2 is ${itemQuan2}<br>`);
+// document.write(`Shipping Charges ${shippingChrg}<br>`);
+// document.write(`Total cost of your order is ${totalCost}`);
+
+// 8
+
+
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var result = (obtainedMarks*100)/totalMarks;
+
+// document.write(`<h1>MarkSheet</h1>`);
+
+// document.write(`Total Marks: ${totalMarks}<br>`);
+// document.write(`Obtained Marks: ${obtainedMarks}<br>`);
+// document.write(`Percentage: ${result}%<br>`);
+
+// 9
+
+// var dollar = 10;
+// var riyal = 25;
+// var total = (dollar* 104.80) + (riyal * 28);
+
+// document.write(`<h1>Currency in PKR</h1>`);
+// document.write(`Total currency in PKR is ${total}`);
+
+// 10
+
+// var num = 12;
+// var  calc = ((num+5)*10)/2;
+
+// document.write(`The result is ${calc}`);
+
+// 11
+
+// var currentYear = 2024;
+// var birthYear = 1996;
+// var age = currentYear-birthYear;
+
+// document.write(`<h1>Age Calculator</h1>`);
+// document.write(`Current Year is ${currentYear}<br>`);
+// document.write(`Birth Year is ${birthYear}<br>`);
+
+// document.write(`your age is ${age}`);
+
+// 12
+// var r = 20;
+// const pi = 3.142;
+// var circum = 2*pi*r;
+// var area = pi*(r**2);
+// document.write(`<h1>The Geometrizer</h1>`);
+// document.write(`Radius of a circle is ${r}<br>`);
+// document.write(`The Circumference is ${circum}<br>`);
+// document.write(`The area is ${area}<br>`);
+
+// 13
+
+var snack = "Chocolate";
+var currentAge = 28;
+var estimatedAge = 70;
+var amountPerday = 20;
+var totalSnack = ((estimatedAge-currentAge)*365)*amountPerday;
+
+document.write(`<h1>The Liftime Supply Calculator</h1>`);
+document.write(`Favourite Snack: ${snack}<br>`);
+document.write(`Current Age: ${currentAge}<br>`);
+document.write(`Estimated Age: ${estimatedAge}<br>`);
+document.write(`Amount of snacks per day: ${amountPerday}<br>`);
+document.write(`You will need ${totalSnack} to last you until the ripe old age of ${estimatedAge}`);
+
